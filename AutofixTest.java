c71d15c8c1219f591dbb2acd3a01c447af128781
Vinayak Sachdeva <vinayak.sachdeva@codenation.co.in>
2019-12-12 22:22:00 +0530

    final commit

--- AutofixTest.java
+++ AutofixTest.java
@@ -149,7 +149,7 @@ public class NewRatingReportCSVExportServletTest {
 
         // Act
         newRatingReportCSVExportServlet.doGet(request, response);
-        check;
+        check2;
 ¬    }
 
 }
