95383ce40e9d96552d5d25be18b473efaa6d772a
Vinayak Sachdeva <vinayak.sachdeva@codenation.co.in>
2020-01-10 20:05:02 +0530

    final commit

--- AutofixTest.java
+++ AutofixTest.java
@@ -132,11 +132,13 @@ public class NewRatingReportCSVExportServletTest {
         String actualBeanName = captor.getValue();
         verify(webApplicationContext, times(1)).getBean(anyString());
         errorCollector.checkThat(actualBeanName,is("standardRatingReportCsvService"));
+        return;
     }
 
     @Test
     public void givenNoReportTypeThenException() throws Exception {
         // Arrange
+        // check
         when(request.getParameter(any())).thenReturn("TestParam");
         RatingReportConfigDTO ratingReportConfigDTO = new RatingReportConfigDTO();
         when(session.getAttribute(anyString())).thenReturn(ratingReportConfigDTO);
